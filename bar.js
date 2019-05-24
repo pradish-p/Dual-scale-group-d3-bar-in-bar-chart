a349f7a8edaa5174588414a990115b79fec57c5f
pradish-p <pradish35@gmail.com>
2019-05-24 22:39:05 +0530

    Update bar.js
    Resolved bar rendering issue.

--- bar.js
+++ bar.js
@@ -2,7 +2,7 @@ var host;
 const margin = {'top':50,'right':50,'bottom':50,'left':50};
 const height = 600 - margin.top - margin.bottom;
 const width = 770 - margin.left - margin.right;
-const barWidths = {'large':48, 'meduim':32, 'small':16};
+const barWidths = {'large':48, 'medium':32, 'small':16};
 var svg;
 var yAxisLeft;
 var yAxisRight;
